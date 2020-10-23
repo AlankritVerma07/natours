0817a1a101c15edd189c866dbd5a7565d2bf7ab0
Alankrit Verma <alankritverma.lko@gmail.com>
2020-10-22 20:47:45 -0700

    Improved Stripe Implementation 4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,8 +14,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    // cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
